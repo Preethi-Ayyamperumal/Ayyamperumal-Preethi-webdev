1a4f1866a2b0f23170bd8228bb5dd67833d3a187
apreethi <preeth.ceg@gmail.com>
2017-07-21 12:04:04 -0400

    3.5 Created Website Service

---
+++ public/assignment/services/website.service.client.js
@@ -0,0 +1,77 @@
+(function () {
+    angular
+        .module("WebAppMaker")
+        .service("WebsiteService", WebsiteService);
+
+    function WebsiteService() {
+
+        var websites = [
+            { "_id": "123", "name": "Facebook",    "developerId": "456", "description": "Lorem" },
+            { "_id": "234", "name": "Tweeter",     "developerId": "456", "description": "Lorem" },
+            { "_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem" },
+            { "_id": "890", "name": "Go",          "developerId": "123", "description": "Lorem" },
+            { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
+            { "_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem" },
+            { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem" }
+        ];
+
+
+        var api={
+            "createUser" : createUser,
+            "findUserById" :findUserById ,
+            "findUserByUsername" :findUserByUsername ,
+            "findUserByCredentials" :findUserByCredentials ,
+            "updateUser" : updateUser,
+            "deleteUser" :deleteUser
+
+        };
+        return api;
+
+        function createWebsite(userId, website) {
+            website._id = (new Date()).getTime() + "";
+            website.developerId=userId;
+            websites.push(website);
+            return website;
+        }
+        function findWebsitesByUser(userId) {
+            var userWebsites=[];
+            for(var w in websites) {
+                if(websites[w].developerId === userId) {
+                    userWebsites.push(websites[w]);
+                }
+            }
+            return userWebsites;
+        }
+        function findWebsiteById(websiteId)  {
+            for(var w in websites) {
+                if(websites[w]._id === websiteId) {
+                    return websites[w];
+                }
+            }
+            return null;
+        }
+        function updateWebsite(websiteId, website)   {
+            for(var w in websites) {
+                if(websites[w]._id === websiteId) {
+                   websites[w] = website;
+                   break;
+                }
+            }
+        }
+
+        function deleteWebsite(websiteId) {
+            var index=-1;
+            for(var w in websites) {
+                if(websites[w]._id === websiteId) {
+                    index = w;
+                    break;
+                }
+            }
+            if (index > -1) {
+                websites.splice(index, 1);
+            }
+        }
+
+
+    }
+})();
