40a14f13a4b4d396e420178c3eea2db7ce4c8257
apreethi <preeth.ceg@gmail.com>
2017-07-21 11:05:43 -0400

    3.3 Configured Angular Routing

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -1,34 +1,74 @@
 (function () {
 
     angular
-        .module("WamApp")
+        .module("WebAppMaker")
         .config(configuration);
 
     function configuration($routeProvider) {
         $routeProvider
             .when("/login", {
-                templateUrl: "views/user/templates/login.view.client.html",
+                templateUrl: "views/user/login.view.client.html",
+                controller: "loginController",
+                controllerAs: "model"
+            })
+            .when("/", {
+                templateUrl: "views/user/login.view.client.html",
                 controller: "loginController",
                 controllerAs: "model"
             })
             .when("/register", {
-                templateUrl: "views/user/templates/register.view.client.html",
+                templateUrl: "views/user/register.view.client.html",
                 controller: "registerController",
                 controllerAs: "model"
             })
-            .when("/profile/:userId", {
-                templateUrl: "views/user/templates/profile.view.client.html",
+            .when("/profile/:uid", {
+                templateUrl: "views/user/profile.view.client.html",
                 controller: "profileController",
                 controllerAs: "model"
             })
         // website routes
-            .when("/user/:userId/website", {
-                templateUrl: "views/website/templates/website-list.view.client.html",
+            .when("/user/:uid/website", {
+                templateUrl: "views/website/website-list.view.client.html",
                 controller: "websiteListController",
                 controllerAs: "model"
             })
-            .when("/user/:userId/website/new", {
-                templateUrl: "views/website/templates/website-new.view.client.html",
+            .when("/user/:uid/website/new", {
+                templateUrl: "views/website/website-new.view.client.html",
+                // controller: "websiteListController",
+                // controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid", {
+                templateUrl: "views/website/website-edit.view.client.html",
+                // controller: "websiteListController",
+                // controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid/page", {
+                templateUrl: "views/page-list.view.client.html",
+                // controller: "websiteListController",
+                // controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid/page/new", {
+                templateUrl: "views/page-new.view.client.html",
+                // controller: "websiteListController",
+                // controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid/page/:pid", {
+                templateUrl: "views/page-edit.view.client.html",
+                // controller: "websiteListController",
+                // controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget", {
+                templateUrl: "views/widget-list.view.client.html",
+                // controller: "websiteListController",
+                // controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/new", {
+                templateUrl: "views/widget-chooser.view.client.html",
+                // controller: "websiteListController",
+                // controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
+                templateUrl: "views/widget-edit.view.client.html",
                 // controller: "websiteListController",
                 // controllerAs: "model"
             })
