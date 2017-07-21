6662a33b3834ed99ca9c9e026ebd278b28ad371f
apreethi <preeth.ceg@gmail.com>
2017-07-21 10:54:59 -0400

    3.2 Created Angular Views/Templates

---
+++ public/assignment/config.js
@@ -0,0 +1,36 @@
+(function () {
+
+    angular
+        .module("WamApp")
+        .config(configuration);
+
+    function configuration($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "views/user/templates/login.view.client.html",
+                controller: "loginController",
+                controllerAs: "model"
+            })
+            .when("/register", {
+                templateUrl: "views/user/templates/register.view.client.html",
+                controller: "registerController",
+                controllerAs: "model"
+            })
+            .when("/profile/:userId", {
+                templateUrl: "views/user/templates/profile.view.client.html",
+                controller: "profileController",
+                controllerAs: "model"
+            })
+        // website routes
+            .when("/user/:userId/website", {
+                templateUrl: "views/website/templates/website-list.view.client.html",
+                controller: "websiteListController",
+                controllerAs: "model"
+            })
+            .when("/user/:userId/website/new", {
+                templateUrl: "views/website/templates/website-new.view.client.html",
+                // controller: "websiteListController",
+                // controllerAs: "model"
+            })
+    }
+})();
