b77848661a59353fe4962de93de205027c079d00
apreethi <preeth.ceg@gmail.com>
2017-07-21 11:51:02 -0400

    3.4 Created User Service

---
+++ public/assignment/services/user.service.client.js
@@ -0,0 +1,82 @@
+(function () {
+    angular
+        .module("WebAppMaker")
+        .service("UserService", UserService);
+
+    function UserService() {
+
+        var users = [
+            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+        ];
+
+        var api={
+            "createUser" : createUser,
+            "findUserById" :findUserById ,
+            "findUserByUsername" :findUserByUsername ,
+            "findUserByCredentials" :findUserByCredentials ,
+            "updateUser" : updateUser,
+            "deleteUser" :deleteUser
+
+        };
+        return api;
+
+        function createUser(user) {
+            if (findUserByUsername(user.username) != null)
+                return null;
+            user._id = (new Date()).getTime() + "";
+            users.push(user);
+            return user;
+        }
+        function findUserById(userId) {
+            for(var u in users) {
+                if(users[u]._id === userId) {
+                    return users[u];
+                }
+            }
+            return null;
+        }
+        function findUserByUsername(username) {
+            for(var u in users) {
+                if(users[u].username === username) {
+                    return users[u];
+                }
+            }
+            return null;
+        }
+        function findUserByCredentials(username, password)  {
+            for(var u in users) {
+                var _user = users[u];
+                if(_user.username === username && _user.password === password) {
+                    return _user;
+                }
+            }
+            return null;
+        }
+        function updateUser(userId, user) {
+            for(var u in users) {
+                if(users[u]._id === userId) {
+                    users[u] = user;
+                    return;
+                }
+            }
+            return null;
+        }
+        function deleteUser(userId) {
+            var index=-1;
+            for(var u in users) {
+                if(users[u]._id === userId) {
+                    index = u;
+                    break;
+                }
+            }
+            if (index > -1) {
+                users.splice(index, 1);
+            }
+        }
+
+
+    }
+})();
